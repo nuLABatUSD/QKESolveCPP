77074d4f8865a0b2e670f446326eaf7e41a51fe2
lf006425 <lfowler@sandiego.edu>
2024-05-28 16:45:39 -0700

    new constants added

--- constants.hh
+++ constants.hh
@@ -1,2 +1,6 @@
 const double _PI_ = 3.141592653589793;
 const double _electron_mass_ = 0.511;
+const double _cos_2theta_ = 0.8;
+const double _sin2theta_ = 0.6;
+const double _GF_ = 11.6;
+const double _delta_m_squared_ = 0.00000005;
