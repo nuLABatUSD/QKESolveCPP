a71f824d22ced074dcd041b23516b80d2311d0e5
lf006425 <lfowler@sandiego.edu>
2024-05-29 16:29:25 -0700

    added functions for vs under three vector, note that they still need work

--- dep_vars.cc
+++ dep_vars.cc
@@ -2,6 +2,7 @@
 #include "arrays.hh"
 #include "constants.hh"
 #include <cmath>
+#include "energy_density_and_pressure.hh"
 
 using std::cout;
 using std::endl;
@@ -136,17 +137,73 @@ void three_vector::set_cross_product(three_vector* A, three_vector* B)
     values[2] = A->get_value(0) * B->get_value(1) - A->get_value(1) * B->get_value(0);
 }
 
+void three_vector::v_vacuum(){
+    values[0] = _delta_m_squared_ / 2 * _sin_2theta_;
+    values[2] = _delta_m_squared_ / 2 * _cos_2theta_;
+}
+
+void three_vector::v_thermal(dummy_vars* q, density* d){
+    
+    three_vector* dummy1 = new three_vector();
+    three_vector* dummy2 = new three_vector();
+    
+    for(int i=0; i<q->get_len(); i++){
+        d->p0_p(i, true, dummy1);
+        d->p0_p(i, false, dummy2);
+
+        values[0] += pow(q->get_val(i),3) * (dummy1->get_value(0) + dummy2->get_value(0));
+        values[1] += pow(q->get_val(i),3) * (dummy1->get_value(1) + dummy2->get_value(1));
+        values[2] += pow(q->get_val(i),3) * (dummy1->get_value(2) + dummy2->get_value(2)); 
+    }
+    delete dummy1;
+    delete dummy2;
+   
+    /*
+    for(int i=0; i<3; i++){
+        values[i] *= -8*sqrt(2)*_GF_/(3*pow(_Z_boson_,2));
+    }
+    
+    double energy_dens = 0;
+    double pressure = 0;
+    energy_and_pressure(_electron_mass_, d->get_T()+1, &energy_dens, &pressure);
+    values[2] += -2 * sqrt(2) * pow(_W_boson_,-2) * _GF_ * (energy_dens+pressure);
+    */
+    
+}
+
+
+
+void three_vector::v_density(dummy_vars* q, density* d){
+    
+    three_vector* dummy1 = new three_vector();
+    three_vector* dummy2 = new three_vector();
+    for(int i=0; i<q->get_len(); i++){
+        d->p0_p(i, true, dummy1);
+        d->p0_p(i, false, dummy2);
+
+        values[0] += pow(q->get_val(i),2) * (dummy1->get_value(0) - dummy2->get_value(0));
+        values[1] += pow(q->get_val(i),2) * (dummy1->get_value(1) - dummy2->get_value(1));
+        values[2] += pow(q->get_val(i),2) * (dummy1->get_value(2) - dummy2->get_value(2));        
+    }
+    delete dummy1;
+    delete dummy2;
+    /*
+    for (int i=0; i<3; i++){
+        values[i] *= sqrt(2)*_GF_ / (2 * pow(_PI_,2));
+    }*/
+}
+
 
 
 
 //density
 
-density::density(int num):dep_vars(8*num)
+density::density(int num):dep_vars(8*num+2)
 {
     N_bins = num;
 }
 
-density::density(linspace* eps, double eta_nu, double eta_mu):dep_vars(8*eps->N)
+density::density(linspace* eps, double eta_nu, double eta_mu):dep_vars(8*eps->N+2)
 {
     N_bins = eps->N;
 
@@ -170,6 +227,10 @@ density::density(linspace* eps, double eta_nu, double eta_mu):dep_vars(8*eps->N)
     
 }
 
+double density::get_T(){
+    return values[N-2];
+}
+
 int density::num_bins()
 {return N_bins;}
 
@@ -181,7 +242,7 @@ void density::p_vector(int t, bool neutrino, three_vector* p)
         }}
     else{
         for(int i=0; i<3; i++){
-            p->set_value(i, values[N/2+4*t+1+i]);
+            p->set_value(i, values[(N-2)/2+4*t+1+i]);
         }}
 }
 
@@ -191,40 +252,19 @@ void density::p0_p(int t, bool neutrino, three_vector* p)
         for(int i=0; i<3; i++){
             p->set_value(i,values[4*t+i+1]);
         }
-        p->multiply_by(values[t]);
+        p->multiply_by(values[4*t]);
         
     }
     
     else{
         for(int i=0; i<3; i++){
-            p->set_value(i, values[N/2+4*t+i+1]);
+            p->set_value(i, values[(N-2)/2+4*t+i+1]);
         }
-        p->multiply_by(values[t]);
+        p->multiply_by(values[4*t+(N-2)/2]);
     }
 }
 
-three_vector density::v_density_integral(dummy_vars* q, density* d){
-    //NOTE CONSTANT TERM NOT INCLUDED!
-    values[0]=0;
-    values[1]=0;
-    values[2]=0;
-    
-    three_vector* dummy1 = new three_vector();
-    three_vector* dummy2 = new three_vector();
-    for(int i=0; i<N; i++){
-        d->p0_p(i, true, dummy1);
-        d->p0_p(i, false, dummy2);
 
-        values[0] += pow(q->get_val(i),2) * (dummy1->get_value(0) - dummy2->get_value(0));
-        values[1] += pow(q->get_val(i),2) * (dummy1->get_value(1) - dummy2->get_value(1));
-        values[2] += pow(q->get_val(i),2) * (dummy1->get_value(2) - dummy2->get_value(2));
-        
-        
-    }
-    delete dummy1;
-    delete dummy2;
-    return values;
-}
 
 
 
