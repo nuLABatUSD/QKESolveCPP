0cced85fb06d7a468702fefb963f7e2f421bf03b
lf006425 <lfowler@sandiego.edu>
2024-06-03 16:57:19 -0700

    new file for matrix class

---
+++ matrices.cc
@@ -0,0 +1,88 @@
+#include "matrices.hh"
+#include <complex>
+#include <iostream>
+
+
+using std::cout;
+using std::endl;
+using std::complex;
+
+
+complex_three_vector::complex_three_vector(int Nv){
+    values = new complex<double>[3]();
+    
+}
+
+complex_three_vector::complex_three_vector(complex<double> x, complex<double> y, complex<double> z){
+    values = new complex<double>[3]();
+    
+    values[0] = x;
+    values[1] = y;
+    values[2] = z;
+    
+}
+
+complex_three_vector::complex_three_vector(complex<double> c){
+    values = new complex<double>[3]();
+    for (int i = 0; i < 3; i++)
+        values[i] = c;
+    
+    
+}
+
+complex_three_vector::complex_three_vector(complex_three_vector* c){
+    values = new complex<double>[3]();
+    for (int i = 0; i < 3; i++)
+        values[i] = c->get_value(i);
+    
+}
+
+void complex_three_vector::print_all(){
+    for (int i=0; i<3; i++){
+        cout << values[i] << endl;
+    }
+    
+}
+
+complex<double> complex_three_vector::get_value(int i){
+    return values[i];
+}
+
+void complex_three_vector::add(complex_three_vector* A, complex_three_vector* B){
+    values[0] = A->get_value(0) + B->get_value(0);
+    values[1] = A->get_value(1) + B->get_value(1);
+    values[2] = A->get_value(2) + B->get_value(2);
+}
+
+complex<double> complex_three_vector::dot_with(complex_three_vector* B)
+{
+    complex<double> dot = 0;
+    for(int i = 0; i < 3; i++)
+        dot += values[i] * B->get_value(i);
+    return dot;
+}
+
+complex<double> complex_three_vector::magnitude_squared()
+{
+    return dot_with(this);
+}
+
+complex<double> complex_three_vector::magnitude()
+{
+    complex<double> sum = 0;
+    for(int i =0; i < 3; i++)
+        sum += pow(this->get_value(i),2);
+    return sqrt(sum);
+}
+
+void complex_three_vector::set_cross_product(complex_three_vector* A, complex_three_vector* B)
+{
+    values[0] = A->get_value(1) * B->get_value(2) - A->get_value(2) * B->get_value(1);
+    values[1] = A->get_value(2) * B->get_value(0) - A->get_value(0) * B->get_value(2);
+    values[2] = A->get_value(0) * B->get_value(1) - A->get_value(1) * B->get_value(0);
+}
+
+complex_three_vector::~complex_three_vector(){
+   delete values; 
+    
+}
