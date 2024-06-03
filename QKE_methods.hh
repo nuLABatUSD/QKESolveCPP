68dae2d266e8d8f5f561c9ba30edf06aaad20d29
lf006425 <lfowler@sandiego.edu>
2024-06-03 16:53:59 -0700

    header file for new QKE_methods.cc file

---
+++ QKE_methods.hh
@@ -0,0 +1,40 @@
+#include "arrays.hh"
+
+class three_vector_for_QKE : public three_vector
+{
+    private:
+    double _delta_m_squared_;
+    double _cos_2theta_;
+    double _sin_2theta_;
+    
+    public:
+
+    three_vector_for_QKE(double, double);
+    
+    void v_vacuum();
+    void v_density(dummy_vars*, density*);
+    void v_thermal(dummy_vars*, density*);
+};
+
+class density : public dep_vars
+{
+    protected:
+    int N_bins;
+    dummy_vars* E;
+    
+    public:
+    
+    density(int, dummy_vars*);
+    density(dummy_vars*, double, double);
+    
+    density(density*);
+    double get_E_value(int);
+    dummy_vars* get_E();
+    double get_T();
+    int num_bins();
+    double p0(int, bool);
+    void p_vector(int, bool, three_vector*);
+    void p0_p(int, bool, three_vector*);
+
+
+};
