9042d7f3713330ba3a53bc5bd0e2e3f80d83e129
lf006425 <lfowler@sandiego.edu>
2024-06-06 16:55:44 -0700

    integrals 1 and 2 working now, F matrix developed

--- test.cc
+++ test.cc
@@ -12,6 +12,9 @@ using std::complex;
 void Fvvsc_components_term_1(density*, bool, int, int, int, double*, three_vector*);
 void Fvvsc_components_term_2(density*, bool, int, int, int, double*, three_vector*);
 void Fvvsc_components(density*, bool, int, int, int, double*, three_vector*);
+void all_F_for_p1(density*, bool, int, double***);
+double F0_integral_one(density*, bool, int, double**);
+double F0_integral_two(density*, bool, int, double**);
 
 int main(){
     
@@ -21,17 +24,32 @@ int main(){
     double eta_mu = -0.02;
     density* den = new density(et, eta_e, eta_mu);
     
-    double* F_03;
-    three_vector* F3 = new three_vector();
-    Fvvsc_components(den, false, 1, 2, 2, F_03, F3);
     
-    cout << "full thing: " << endl;
-    cout << "F0: " << *F_03 << endl << "F: ";    
-    F3->print_all();
+    double*** F_values = new double**[4];
+    for(int i=0; i<4; i++){
+        F_values[i] = new double*[den->num_bins()];
+        for(int j=0; j<den->num_bins(); j++){
+            F_values[i][j] = new double[den->num_bins()];   
+        }
+    }
+    
+    
+    all_F_for_p1(den, false, 1, F_values);
+
+    
+    cout << F0_integral_two(den, false, 5, F_values[0]);
+    
+    for(int i=0; i<4; i++){
+        for(int j=0; j<den->num_bins(); j++){
+            delete[] F_values[i][j];
+        }
+        delete[] F_values[i];
+    }
+    delete[] F_values;
+    
     
     delete et;
     delete den;
-    delete F3;
     
     return 0;
 }
@@ -178,6 +196,7 @@ void Fvvsc_components_term_2(density* dens, bool neutrino, int p1, int p2, int p
     delete p_4;
 }
 
+//Note: this function has no safeguard for negative p4, will produce a result with no error
 void Fvvsc_components(density* dens, bool neutrino, int p1, int p2, int p3, double* F03, three_vector* F3){
     
     double F01;
@@ -189,13 +208,6 @@ void Fvvsc_components(density* dens, bool neutrino, int p1, int p2, int p3, doub
     Fvvsc_components_term_1(dens, neutrino, p1, p2, p3, &F01, F1);
     Fvvsc_components_term_2(dens, neutrino, p1, p2, p3, &F02, F2);
     
-    cout << "term 1: " << endl;
-    cout << "F0: " << std::setprecision (60) << F01 << endl << "F: ";    
-    F1->print_all();
-    cout << "term 2: " << endl;
-    cout << "F0: " << F02 << endl << "F: ";    
-    F2->print_all();
-    
     F2->multiply_by(-1);
     F3->add(F1, F2);
     
@@ -205,142 +217,116 @@ void Fvvsc_components(density* dens, bool neutrino, int p1, int p2, int p3, doub
     delete F2;
 }
 
-
-/* 
-
-F_vals is N*N length dep vars object (this comes from there being N options for p2 and N options for p3 so N^2 total combinations
-there is one F_vals object for every p1 value
-
-the dep_vars object is ordered first by p2, then by p3--so it looks like {(val for p1=0,p2=0), (val for p1=0,p2=1), ... (val for p1=0,p2=N), (val for p1=1,p2=0), ... (val for p1=N,p2=N)}
-each val in and of itself is four values
-
-so to get the F0 value for p2=i and p3=j find F_vals->get_value(i*N+j)
-to get the Fx value find F_vals->get_value(i*N+j+1)
-to get the Fy value find F_vals->get_value(i*N+j+2)
-to get the Fz value find F_vals->get_value(i*N+j+3)
-
-Note: it is already build into Fvvsc_components to set rho_4 to 0 when p4 is outside max energy
-
+/*
+populates the double*** object F_vals, which is a 4 by eps->N by eps->N matrix
+F0: index is [0][p2][p3]
+Fx: index is [1][p2][p3]
+Fy: index is [2][p2][p3]
+Fz: index is [3][p2][p3]
 */
 
-/*
-void all_F_for_p1(density* dens, bool neutrino, int p1, dep_vars* F_vals){
+void all_F_for_p1(density* dens, bool neutrino, int p1, double*** F_vals){
     dummy_vars* eps = dens->get_E();
     
-    double* F0;
+    double F0 = 0;
     three_vector* Fxyz = new three_vector();
     
     for(int p2=0; p2<eps->N; p2++){
-        for (int p3=0; p3<eps->N; p3++){
-            Fvvsc_components(dens, neutrino, p1, p2, p3, F0, Fxyz);
-
-            F_vals->set_value(p2*eps->N + p3, *F0);
-            F_vals->set_value(p2*eps->N + p3 + 1, Fxyz->get_value(0));
-            F_vals->set_value(p2*eps->N + p3 + 2, Fxyz->get_value(1));
-            F_vals->set_value(p2*eps->N + p3 + 3, Fxyz->get_value(2));
+        for(int p3=0; p3<eps->N; p3++){
+            
+            if(p1+p2-p3>=0){
+                Fvvsc_components(dens, neutrino, p1, p2, p3, &F0, Fxyz);
+                F_vals[0][p2][p3] = F0;
+                F_vals[1][p2][p3] = Fxyz->get_value(0);
+                F_vals[2][p2][p3] = Fxyz->get_value(1);
+                F_vals[3][p2][p3] = Fxyz->get_value(2);
+            }
         }
-        
-    }
+    }  
     
     delete Fxyz;
 }
-*/
 
-double K1(double p1, double p3){
-    return 16./15 * pow(p3, 3) * (10 * pow(p1, 2) - 5 * p1 * p3 + pow(p3, 2));  
+
+double J1(double p1, double p2, double p3){
+    return 16./15 * pow(p3, 3) * (10 * pow(p1+p2, 2) - 15 * (p1+p2) * p3 + 6*pow(p3, 2));  
 }
 
-double K2(double p1, double p2, double p3){
-    return 16./15 * pow(p2, 3) * (10 * pow(p1-p3, 2) + 15 * (p1-p3) * p3 + 6*pow(p2, 2));  
+double J2(double p1, double p2){
+    return 16./15 * pow(p2, 3) * (10 * pow(p1, 2) + 5 * p1 * p2 + pow(p2, 2));  
 }
 
-double K3(double p1, double p2, double p3){
-    return 16./15 * (pow(p1-p3, 5) + 10 * pow(p1-p3, 2) * pow(p2, 3) + 15 * (p1-p3) * pow(p2, 4) + 6 * pow(p2,5));
+double J3(double p1, double p2, double p3){
+    return 16./15 * (pow(p1+p2, 5) - 10 * pow(p1+p2, 2) * pow(p3, 3) + 15 * (p1+p2) * pow(p3, 4) - 6 * pow(p3,5));
 }
 
-/*
-double F0_integral(density* dens, bool neutrino, int p1){
+
+double F0_integral_one(density* dens, bool neutrino, int p1, double** F_vals){
     dummy_vars* eps = dens->get_E();
-    linspace_for_trap* p_2 = new linspace_for_trap(0, eps->get_value(p1), eps->N);
+    linspace_for_trap* p_2 = new linspace_for_trap(0, eps->get_value(p1), p1+1);
+    double p_1_energy = eps->get_value(p1);
     
-    double* F0;
-    three_vector* F = new three_vector();
-    double interior_integrals = 0;
+    dep_vars* dummy_p_2 = new dep_vars(p1+1);
     
-    linspace_for_trap* I1_domain = new linspace_for_trap(0, eps->get_value(p2), p2+1);
-    
-    
-    dep_vars* dummy_p_3 = new dep_vars(p2+1);
-    for(int i=0; i<eps->N; i++){
-        dummy_p_3->set_value(i) = F0 * K1(p1,eps->get_value(i));
-    }
-    interior_integrals += I1_domain->integrate(dummy_p_3);
-    
-    for(int i=p2; i<p1; i++){
+    for(int p2=1; p2<=p1; p2++){
+        linspace_for_trap* I1_domain = new linspace_for_trap(0, eps->get_value(p2), p2+1);
+ 
+        dep_vars* dummy_p_3 = new dep_vars(p2+1);
+        
+        for(int p3=0; p3<=p2; p3++){
+            dummy_p_3->set_value(p3, F_vals[p2][p3] * J1(p_1_energy, eps->get_value(p2), eps->get_value(p3)));
+        }
+        dummy_p_2->set_value(p2, I1_domain->integrate(dummy_p_3));
+        
+        delete dummy_p_3;
+        delete I1_domain;
         
     }
     
-                                          
+    double result = p_2->integrate(dummy_p_2);
     
+    delete dummy_p_2;
+    delete p_2;
     
+    return result;
 }
-*/
 
-/*
 
-void integral_one(density* dens, bool neutrino, int p1, double* sumF0, double* sumFx, double* sumFy, double* sumFz){
+//input double** F_vals is 201 by 201 matrix of p2/p3 F values for a particular one of F0, Fx, Fy, Fz
+double F0_integral_two(density* dens, bool neutrino, int p1, double** F_vals){
     dummy_vars* eps = dens->get_E();
-    double p1_energy = eps->get_value(p1);
+    double p_1_energy = eps->get_value(p1);
+    linspace_for_trap* p_2 = new linspace_for_trap(0, p_1_energy, p1+1);
+    double max_energy = eps->get_value(eps->N-1);
     
-    double* F01;
+    dep_vars* dummy_p_2 = new dep_vars(p1+1);
     
-    three_vector* F1 = new three_vector();
-    
+    //NOTE: P2 GOES UP TO P1 NOT INCLUDING P1 BC FOR P2=P1 INNER INTEGRAL WILL BE 0
     for(int p2=0; p2<p1; p2++){
         
-        for(int p3=0; p3<p2; p3++){
-            //Fvvsc_components(dens, neutrino, p1, p2, p3, F01, F1);
-            
-            double p3_energy = eps->get_value(p3);
-            *sumF0 += *F01 + 16./15. * pow(p3_energy,3) * (10 * pow(p1_energy,2) - 5 * p1_energy * p3_energy + pow(p3_energy,2));
-            *sumFx += F1->get_value(0) + 16./15. * pow(p3_energy,3) * (10 * pow(p1_energy,2) - 5 * p1_energy * p3_energy + pow(p3_energy,2));
-            *sumFy += F1->get_value(1) + 16./15. * pow(p3_energy,3) * (10 * pow(p1_energy,2) - 5 * p1_energy * p3_energy + pow(p3_energy,2));
-            *sumFz += F1->get_value(2) + 16./15. * pow(p3_energy,3) * (10 * pow(p1_energy,2) - 5 * p1_energy * p3_energy + pow(p3_energy,2));
-        }
-        
-        
-        for(int p3=p2; p3<p1; p3++){
-            //Fvvsc_components(dens, neutrino, p1, p2, p3, F01, F1);
-            
-            double p2_energy = eps->get_value(p2);
-            double p3_energy = eps->get_value(p3);
-            
-            *sumF0 += *F01 + 16./15. * pow(p2_energy,2) * (10 * pow(p1_energy - p2_energy,2) + 15 * (p1_energy - p3_energy) * p2_energy + 6 * pow(p2_energy,2));
-            *sumFx += F1->get_value(0) + 16./15. * pow(p2_energy,2) * (10 * pow(p1_energy - p2_energy,2) + 15 * (p1_energy - p3_energy) * p2_energy + 6 * pow(p2_energy,2));
-            *sumFy += F1->get_value(1) + 16./15. * pow(p2_energy,2) * (10 * pow(p1_energy - p2_energy,2) + 15 * (p1_energy - p3_energy) * p2_energy + 6 * pow(p2_energy,2));
-            *sumFz += F1->get_value(2) + 16./15. * pow(p2_energy,2) * (10 * pow(p1_energy - p2_energy,2) + 15 * (p1_energy - p3_energy) * p2_energy + 6 * pow(p2_energy,2));
-            
-        }
-        
-        for(int p3=p1; p3<p1+p2; p3++){
-            //Fvvsc_components(dens, neutrino, p1, p2, p3, F01, F1);
-            
-            double p2_energy = eps->get_value(p2);
-            double p3_energy = eps->get_value(p3);
-            
-            *sumF0 += *F01 + 16./15. * (pow(p1_energy - p3_energy,5) + 10 * pow(p1_energy - p3_energy,2) * pow(p2_energy,3) + 15 * (p1_energy - p3_energy) * pow(p2_energy,4) + 6 * pow(p2_energy,5));
-            *sumFx += F1->get_value(0) + 16./15. * (pow(p1_energy - p3_energy,5) + 10 * pow(p1_energy - p3_energy,2) * pow(p2_energy,3) + 15 * (p1_energy - p3_energy) * pow(p2_energy,4) + 6 * pow(p2_energy,5));
-            *sumFy += F1->get_value(1) + 16./15. * (pow(p1_energy - p3_energy,5) + 10 * pow(p1_energy - p3_energy,2) * pow(p2_energy,3) + 15 * (p1_energy - p3_energy) * pow(p2_energy,4) + 6 * pow(p2_energy,5));
-            *sumFz += F1->get_value(2) + 16./15. * (pow(p1_energy - p3_energy,5) + 10 * pow(p1_energy - p3_energy,2) * pow(p2_energy,3) + 15 * (p1_energy - p3_energy) * pow(p2_energy,4) + 6 * pow(p2_energy,5));
-        }
-    }
-    delete F1;
-    
-    
-    *sumF0 *= pow(_GF_,2) / (4 * pow(2*_PI_, 3) * pow(p1_energy,2));
-    *sumFx *= pow(_GF_,2) / (4 * pow(2*_PI_, 3) * pow(p1_energy,2));
-    *sumFy *= pow(_GF_,2) / (4 * pow(2*_PI_, 3) * pow(p1_energy,2));
-    *sumFz *= pow(_GF_,2) / (4 * pow(2*_PI_, 3) * pow(p1_energy,2));
+        linspace_for_trap* I2_domain = new linspace_for_trap(eps->get_value(p2), p_1_energy, p1-p2+1);
 
+        
+        dep_vars* dummy_p_3 = new dep_vars(p1-p2+1);
+        
+        //NOTE: INTEGRAL IS SET TO 0 FOR ALL P3 GREATER THAN PMAX
+        for(int p3=p2; (p3<=p1) and (p3<=max_energy); p3++){
+            dummy_p_3->set_value(p3-p2, F_vals[p2][p3] * J2(p_1_energy, eps->get_value(p2)));
+            
+        }
+        
+        dummy_p_2->set_value(p2, I2_domain->integrate(dummy_p_3));
+        
+        
+        
+        delete dummy_p_3;
+        delete I2_domain;
+    }
+
+    double result = p_2->integrate(dummy_p_2);
+    
+    delete dummy_p_2;
+    delete p_2;
+    
+    return result;
 }
-*/
