d22a6412c4206b9a9525768ca5afc8ca174f0ce9
lf006425 <lfowler@sandiego.edu>
2024-07-25 09:06:29 -0700

    updated

--- test3.cc
+++ test3.cc
@@ -2,25 +2,54 @@
 #include "arrays.hh"
 #include "QKE_methods.hh"
 
+using std::cout;
+using std::endl;
+
 int main(){
     linspace_and_gl* et = new linspace_and_gl(0., 10., 201, 5);
     double eta_e = 0.01;
     double eta_mu = -0.01;
-    
+
     density* den1 = new density(et, eta_e, eta_mu);
     density* den2 = new density(den1->num_bins(), et);
-    integration* int0 = new integration(et, 1);
+    integration* test_int = new integration(et, 9);
     double* int_vals = new double[4];
-    int0->whole_integral(den1, true, int_vals);
+    test_int->whole_integral(den1, true, int_vals);
+    cout << "neutrino results" << endl;
     for(int i=0; i<4; i++){
-        std::cout << int_vals[i] << std::endl;
+        cout << int_vals[i] << endl;
     }
+    test_int->whole_integral(den1, false, int_vals);
+    cout << "antineutrino results" << endl;
+    for(int i=0; i<4; i++){
+        cout << int_vals[i] << endl;
+    }
+
+    three_vector* v = new three_vector();
+    den1->p_vector(206, false, v);
+    v->print_all();
+   
+    cout << den1->p0(205, false) << endl;
+
     
+    double* dummy_int = new double[4];
+    integration* test_int2 = new integration(et, 9);
+    test_int2->whole_integral(den1, false, dummy_int);
+    cout << "when processor 0 tries the integral" << endl;
+    for(int j=0; j<4; j++){
+        cout << dummy_int[j] << endl;
+    }
+    delete[] dummy_int;
+    delete test_int2;
+
+
     delete den2;
     delete den1;
     delete et;
-    delete int0;
+    delete v;
+    delete test_int;
     delete[] int_vals;
     
     return 0;
+    
 }
