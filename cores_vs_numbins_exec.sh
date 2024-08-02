64b3946cbf606fa1c3fd5afe5a63977f6bf75170
lf006425 <lfowler@sandiego.edu>
2024-08-02 09:45:54 -0700

    updated to overwrite file on first run

--- cores_vs_numbins_exec.sh
+++ cores_vs_numbins_exec.sh
@@ -4,7 +4,7 @@ set -x
 
 mpic++ test2.cc QKESolveMPI.cc array_methods.cc QKE_methods.cc thermodynamics.cc matrices.cc QKESolve.cc -std=c++11 -o wed
 
-mpiexec -n 4 wed | tee -a output.txt
+mpiexec -n 4 wed | tee output.txt
 
 mpiexec -n 5 wed | tee -a output.txt
 
