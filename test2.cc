b57d160424e1addb0d0681f1489862e8ed485aaa
lf006425 <lfowler@sandiego.edu>
2024-08-05 10:42:48 -0700

    updated

--- test2.cc
+++ test2.cc
@@ -27,10 +27,45 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
     
     linspace_and_gl* et = new linspace_and_gl(0., 10., 201, 5);
-    double eta_e = 0.01;
-    double eta_mu = -0.01;
+    eta_e = i*0.01;
+    eta_mu = -0.01;
+
+    QKESolveMPI* sim1 = new QKESolveMPI(myid, numprocs, et, 0.8, 2.5e-15, eta_e, eta_mu);
+    density* den1 = new density(et, eta_e, eta_mu);
+    density* den2 = new density(den1);
+    den1->set_T(0.25);
+    sim1->set_ics(0, den1, 1.e12);
+    
+    double average_time_elapsed = 0;
+    for(int i=0; i<3; i++){
+        auto start = high_resolution_clock::now();
+        sim1->f(1, den1, den2);
+
+        //sim1->run(2, 1, 5.e15,"QKE1.csv", true);
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(stop - start);
+        double time_elapsed = duration.count()/1000.;
+
+        double max_time_elapsed = 0;
+        MPI_Reduce(&time_elapsed, &max_time_elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+        if(myid == 0){
+            cout << "eta_e=" << eta_e << ", eta_mu=" << eta_mu << " time elapsed: " << max_time_elapsed << endl;
+        }
+        average_time_elapsed += max_time_elapsed;
+        
+        
+    }
+    average_time_elapsed /= 3;
+    if(myid==0){
+        cout << "Average time elapsed with " << numprocs << " processors is " << average_time_elapsed << endl;}
+    
+    delete sim1;
+    delete den1;
+    delete den2;
     
     
+    
+    /*
     double average_time_elapsed = 0;
     for(int i=-10; i<11; i++){
         eta_e = i * 0.01;
@@ -69,7 +104,7 @@ int main(int argc, char *argv[])
     average_time_elapsed /= 21;
     if(myid==0){
         cout << "Average time elapsed with " << numprocs << " processors is " << average_time_elapsed << endl;}
-    
+    */
     
     
     delete et;
