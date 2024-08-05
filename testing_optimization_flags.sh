2117a573a4da35191ea473d697eb652eeefaad76
lf006425 <lfowler@sandiego.edu>
2024-08-05 11:00:35 -0700

    updated

--- testing_optimization_flags.sh
+++ testing_optimization_flags.sh
@@ -8,12 +8,12 @@ mpiexec -n 8 wed | tee outputo0.txt
 
 mpic++ test3.cc QKESolveMPI.cc array_methods.cc QKE_methods.cc thermodynamics.cc matrices.cc QKESolve.cc -std=c++11 -o1 wed
 
-mpiexec -n 5 wed | tee outputo1.txt
+mpiexec -n 8 wed | tee outputo1.txt
 
 mpic++ test3.cc QKESolveMPI.cc array_methods.cc QKE_methods.cc thermodynamics.cc matrices.cc QKESolve.cc -std=c++11 -o2 wed
 
-mpiexec -n 5 wed | tee outputo2.txt
+mpiexec -n 8 wed | tee outputo2.txt
 
 mpic++ test3.cc QKESolveMPI.cc array_methods.cc QKE_methods.cc thermodynamics.cc matrices.cc QKESolve.cc -std=c++11 -o3 wed
 
-mpiexec -n 5 wed | tee outputo3.txt
+mpiexec -n 8 wed | tee outputo3.txt
