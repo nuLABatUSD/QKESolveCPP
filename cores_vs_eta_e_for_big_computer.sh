bafaf82b9b1449a5f76c1b11f9a3b3b58522514b
lf006425 <lfowler@sandiego.edu>
2024-08-05 14:45:59 -0700

    new test file

---
+++ cores_vs_eta_e_for_big_computer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+set -x
+
+mpic++ test4.cc QKESolveMPI.cc array_methods.cc QKE_methods.cc thermodynamics.cc matrices.cc QKESolve.cc -std=c++11 -o wed
+
+mpiexec -n 4 wed opposite "4coreoutputs.csv" "4coretimes.csv"
+
+mpiexec -n 16 wed opposite "16coreoutputs.csv" "16coretimes.csv"
+
+mpiexec -n 32 wed opposite "32coreoutputs.csv" "32coretimes.csv"
+
+mpiexec -n 64 wed opposite "64coreoutputs.csv" "64coretimes.csv"
