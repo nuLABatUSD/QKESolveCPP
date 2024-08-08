4ba5f8234e4aa44977e0235777976fe5560889f9
lf006425 <lfowler@sandiego.edu>
2024-08-08 11:46:51 -0700

    new tests for anvil

--- cores_vs_eta_e_official_test_128_cores.sh
+++ cores_vs_eta_e_official_test_128_cores.sh
@@ -14,19 +14,4 @@
 module purge
 module load modtree/cpu
 
-
-xmin="0."
-xmax="10."
-numlin="201"
-numgl="5"
-eta_e="0.01"
-eta_mu="-0.01"
-sin2theta="0.8"
-deltamsquared="2.5e-15"
-x_initial="0."
-dx_initial="1.e12"
-eta_relationship="same"
-outputs="f_results_128_cores.csv"
-times="times_128_cores.csv"
-
-mpiexec -n 128 wed $xmin $xmax $numlin $numgl $eta_e $eta_mu $sin2theta $deltamsquared $x_initial $dx_initial $eta_relationship $ouputs $times
+mpiexec -n 128 wed 0. 10. 201 5 0.01 -0.01 0.8 2.5e-15 0. 1.e12 same "results_128_cores.csv" "times_128_cores.csv"
