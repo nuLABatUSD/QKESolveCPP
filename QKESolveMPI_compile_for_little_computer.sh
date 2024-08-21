165167fc6467c1bc0c804db14a1715ae85c20b0d
lf006425 <lfowler@sandiego.edu>
2024-08-21 09:46:14 -0700

    new test for pc

--- QKESolveMPI_compile_for_little_computer.sh
+++ QKESolveMPI_compile_for_little_computer.sh
@@ -10,20 +10,20 @@ numlin="201"
 numgl="5"
 sin2theta="0"
 deltamsquared="0"
-A="1"
-B="8"
+eta_e="0.01"
+eta_mu="-0.01"
 T="0.25"
-N_step="5"
+N_step="2"
 dN="1"
 x_initial="0."
 x_final="5.e15"
 dx_initial="1.e12"
 verbose="true"
-output_file="not_equilibrium_QKESolveMPI_results.csv"
+output_file="electron_and_neutrino_together_QKESolveMPI_results.csv"
 input_file="initialdensity.csv"
 
-g++ initialize_dens.cc array_methods.cc matrices.cc thermodynamics.cc QKE_methods.cc -std=c++11 -o makedens
-./makedens $xmin $xmax $numlin $numgl $T $A $B $input_file
+g++ initialize_thermal_dens.cc array_methods.cc matrices.cc thermodynamics.cc QKE_methods.cc -std=c++11 -o makedens
+./makedens $xmin $xmax $numlin $numgl $T $eta_e $eta_mu $input_file
 
 mpic++ static_main_QKESolveMPI.cc QKESolveMPI.cc array_methods.cc QKE_methods.cc thermodynamics.cc matrices.cc QKESolve.cc -std=c++11 -o wed
 
