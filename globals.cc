f607eb07dcc694431c7d0379b0b36f6816139f2e
lf006425 <lfowler@sandiego.edu>
2024-08-27 14:48:06 -0700

    post processing code

--- globals.cc
+++
@@ -1,8 +0,0 @@
-#include "QKESolve.hh"
-#include "globals.hh"
-
-namespace Globals {
-// all of these global variables are set at the start of main():
-int myid;         // MPI rank of this process
-int numprocs;          // total number of MPI ranks
-}
--- globals.hh
+++
@@ -1,8 +0,0 @@
-#ifndef GLOBALS_HH_
-#define GLOBALS_HH_
-
-namespace Globals {
-extern int myid, numprocs;
-}
-
-#endif // GLOBALS_HH_
